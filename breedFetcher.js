8357e2b4d2a762dd25a51696bb787c93cec2bbeb
Kevin Levasseur <kevin_levasseur@telus.net>
2024-01-24 13:36:19 -0800

    refactored, passes data to index file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,24 @@
 const request = require('request');
 
-const breed = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  let errorValue = null;
+  let descriptionValue = null;
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
-  if (error) {
-    console.log("❌ Invalid URL"); // If the URL doesn't work, prints a message to console.
-  } else if (response.statusCode !== 200) {
-    console.log("❌ Check your URL. Error code: ", response.statusCode); // if the URL only partially works, prints an error code
-  } else {
-    const data = JSON.parse(body);
-    if (data[0]) { // if the data exists print the data!
-      console.log(data[0]);
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error) {
+      errorValue = "❌ Invalid URL"; // If the URL doesn't work, prints a message to console.
+    } else if (response.statusCode !== 200) {
+      errorValue = `❌ Check your URL. Error code: ${response.statusCode}`; // if the URL only partially works, prints an error code
     } else {
-      console.log("❌ Breed not found! 😿"); // message printed when no data found for breed
+      const data = JSON.parse(body);
+      if (data[0]) { // if the data exists print the data!
+        descriptionValue = data[0];
+      } else {
+        errorValue = "❌ Breed not found! 😿"; // message printed when no data found for breed
+      }
     }
-  }
-});
+    callback(errorValue, descriptionValue); // passes our error or description back to index.js
+  });
+}
+
+module.exports = { fetchBreedDescription };
